9eb257f6ae2b4279be6e53299ecb4a134931c6d9
Rabbidon <edwinfennell1@gmail.com>
2019-05-14 15:25:02 +0100

    Hangman initial commit

---
+++ Distributed Systems Simulator/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Distributed_Systems_Simulator
+{
+    class Program
+    {
+        // This simulates a basic server.
+        // Our clients will send requests to the server in the form of integers.
+        // The server will then add a fixed integer to the integer provided by the client, and send it back to the original user.
+        class Server
+        {
+        }
+
+        class Client
+        {
+        }
+
+        public static void main();
+}
--- Distributed Systems Simulator/README.md
+++ Distributed Systems Simulator/README.md
@@ -1,1 +1,3 @@
 The aim here is to make a framework that can simulate computer networks for testing purposes etc.
+
+To get a system with an acceptable level of functionality will likely take a while - you should see quite a few implementations in here over time, starting off simple and increasing in complexity as I work out what I am actually doing.
